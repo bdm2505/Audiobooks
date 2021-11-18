7626f17e176b4069e2cef5e2a3e6711eead2e09d
Дмитрий Башкирцев <bdm1999@yandex.ru>
2021-11-18 23:27:55 +0500

    add bookActivity

--- app/src/main/java/ru/lytvest/audiobooks/BookActivity.kt
+++ app/src/main/java/ru/lytvest/audiobooks/BookActivity.kt
@@ -2,10 +2,31 @@ package ru.lytvest.audiobooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
+
+        book?.let {
+            val name = findViewById<TextView>(R.id.name_book)
+            name.text = it.name
+            findViewById<TextView>(R.id.author_book).text = it.author
+            findViewById<TextView>(R.id.description_book).text = it.description
+            Glide.with(this)
+                .load(it.image)
+                .placeholder(R.drawable.load)
+                .error(R.drawable.ic_launcher_background)
+                .into(findViewById(R.id.image_book))
+        } ?: run {
+            finish()
+        }
+    }
+
+    companion object {
+        var book: Book? = null
     }
 }
